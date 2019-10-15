6abceba69384b813dc3608a11e2580b1ca0942a8
Zulfiia Salakhutdinova <zulfiia@MacBook-Pro-Zulfiia.local>
2019-10-15 12:33:38 +0400

    add env configuration

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react"
-import 'styles/App.css';
+import 'styles/App.css'
 
 class App extends Component {
   render() {
+    console.log(process.env)
     return (
         <div>
             <h1>My React App! Test</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // NOTE: плагин генерирует HTML файл, затем вставляет в него скрипт и записывает на dist/index.html
+const webpack = require('webpack');
+const env = require('dotenv').config({path: __dirname + '/.env'}).parsed;
 
 module.exports = {
   entry: './src/index.js',
@@ -7,6 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: "index_bundle.js",
   },
+  devServer: {
+    port: env.PORT || 3000
+  },  
   module: {
     rules: [
       {
@@ -25,9 +30,26 @@ module.exports = {
       }
     ],  
   },
+  resolve: {
+    modules: [path.join(__dirname, env.NODE_PATH || "src"), "node_modules"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
-    })
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(env || {})
+    })    
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_PATH: 'src/',
+    //     PORT: 3456,
+    //     ...dotenv.parsed
+    //   },
+    // }),    
+    // new Dotenv({
+    //   path: './.env', // Path to .env file (this is the default)
+    //   safe: true // load .env.example (defaults to "false" which does not use dotenv-safe)
+    // })
   ]    
 };
